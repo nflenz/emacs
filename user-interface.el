720adc349e35186dd4b084571f9ec16364a4b73f
Nicholas Lenz <Nicholas Lenz>
2026-08-24 22:56:14 -0400

    Disable tool-bar-mode and scroll-bar-mode

--- user-interface.el
+++ user-interface.el
@@ -5,8 +5,8 @@
 
 ;; Unwanted GUI features
 (menu-bar-mode -1)
-(when (display-graphic-p) (tool-bar-mode -1))
-(when (display-graphic-p) (scroll-bar-mode -1))
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
 
 ;; Use y/n instead of yes/no
 (defalias 'yes-or-no-p 'y-or-n-p)
